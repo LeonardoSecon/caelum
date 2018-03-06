ecaa8218380759314eb89301d12d59c6a8d94180
leonardosecon <angular7508@caelum141-05>
2018-03-06 20:31:08 -0300

    Aula 1 space to test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector:'app-root',
+templateUrl:'./app.component.html',
+styles:['h1{color:red; text-decoration:underline;background-color:yellow; }']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent{
+
+
+
+
+
 }
+
+
+
+
+
+
+
+//import { Component } from '@angular/core';
+
+
+
+
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'app';
+// }
